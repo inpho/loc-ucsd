ce9f7d156fa206e6152932325cbecae63dc6ce21
Jaimie Murdock <jaimie.murdock@gmail.com>
2013-12-13 15:27:47 -0500

    pulling down the proper LCC file

--- fetch-data.sh
+++ fetch-data.sh
@@ -8,7 +8,7 @@ wget -NP data http://id.loc.gov/static/data/authoritiessubjects.nt.skos.zip
 unzip -fo data/authoritiessubjects.nt.skos.zip -d data
 
 # LCC Dataset from GitHub
-wget -NP data https://github.com/edsu/lcco/blob/master/lcco.rdf
+wget -NP data https://raw.github.com/edsu/lcco/master/lcco.rdf
 
 # UCSD Data
 wget -NP data http://sci.cns.iu.edu/ucsdmap/data/UCSDmap.net 
