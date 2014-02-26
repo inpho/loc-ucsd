0f9ce8c95496719f2fb9bceed7884d56a2401309
Jaimie Murdock <jaimie.murdock@gmail.com>
2014-02-26 16:21:46 -0500

    base file

--- htrc/mapping.py
+++ htrc/mapping.py
@@ -1,4 +1,3 @@
-import htrc
 """
 The stuff in lcc is all relative to local file paths that may or may not exist
 anymore. Probably not. Defintiely not.
@@ -7,18 +6,38 @@ Still, the code is correct for doing the actual parsing once you get the file
 data. Some of it is path dependent, some is data dependent, edit lcc to make it
 data dependent, rather than path dependent.
 """
+
+from collections import defaultdict
+import htrc
 import lcc
 
 #open up 'data/htrc/1315.csv', '86.csv', '6.csv', etc.
+with open(volumefile) as f:
+    for line in f:
+        find_ucsd_mappings(line)
+
+ucsd = generate_lcc_ucsd_map()
 
 #pseudo-code
 def find_ucsd_mappings(id):
-    metadata = htrc.metadata(id)
+    """ Takes an ID and returns a list of ucsd classifications """
+    # lookup the metadata for the volume
+    metadata = htrc.metadata(id) #this just works
+
     # do some stuff to find the LCCN - look at lcc.py
     lccn = lcc.get_lccn(metadata) # demo data
 
     # look up the LCC using lcco.py
-    lcc.get_lccs(lccns)
+    classification = lcc.get_lccs(lccns)
 
-    # look up the UCSD mapping using 'data/csv/lcco_ucsd.csv'
-    return ucsd[lcc]
+    # using the global dictionary to return the subd_ids
+    return ucsd[classification]
+
+def generate_lcc_ucsd_map():
+    # look up the UCSD mapping using '../data/csv/lcco_ucsd.csv'
+    lcc = defaultdict(list)
+
+    for row in mapfile:
+        lcc[row[LoC]].append(row[subd_id])
+
+    return lcc
