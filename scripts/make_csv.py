ce726fdad21ccb733ff66edbc252ae6ba9299037
Jaimie Murdock <jaimie.murdock@gmail.com>
2014-02-28 15:53:42 -0500

    adding manual lookup table for first 86 volumes

--- scripts/make_csv.py
+++ scripts/make_csv.py
@@ -3,29 +3,40 @@ import json
 
 htrc1314 = []
 htrc86 = []
-htrc6=["uc2.ark+=13960=t5w66bs1h\n",
-"uc2.ark+=13960=t6057f659\n",
-"uc2.ark+=13960=t74t6gs0m\n",
-"uc2.ark+=13960=t0ht2h954\n",
-"uc2.ark+=13960=t05x26n0d\n",
-"uc2.ark+=13960=t5p84550z\n"]
+htrc6 = []
+volumes6=["uc2.ark+=13960=t5w66bs1h",
+"uc2.ark+=13960=t6057f659",
+"uc2.ark+=13960=t74t6gs0m",
+"uc2.ark+=13960=t0ht2h954",
+"uc2.ark+=13960=t05x26n0d",
+"uc2.ark+=13960=t5p84550z"]
 
 with open('htrc86.json') as jsonfile:
     data = json.load(jsonfile)
-    volumes = data.keys()
+    volumes86 = data.keys()
 
 with open('../data/csv/htrc_lcco.csv', 'rb') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        htrc1314.append(row['id'] + '\n')
-        if row['id'] in volumes:
-            htrc86.append(row['id'] + '\n')
+        if row['id'] in volumes6:
+            htrc6.append({'id' : row['id'], 
+                          'lccn' : row['id2'],
+                          'collection' : 'htrc6'})
+        elif row['id'] in volumes86:
+            htrc86.append({'id' : row['id'], 
+                           'lccn' : row['id2'],
+                           'collection' : 'htrc86'})
+        else:
+            htrc1314.append({'id' : row['id'], 
+                             'lccn' : row['id2'],
+                             'collection': 'htrc1314'})
 
 print len(htrc1314), len(htrc86), len(htrc6)
 
-with open('../data/htrc/1314.txt', 'w') as txtfile:
-    txtfile.writelines(htrc1314)
-with open('../data/htrc/86.txt', 'w') as txtfile:
-    txtfile.writelines(htrc86)
-with open('../data/htrc/6.txt', 'w') as txtfile:
-    txtfile.writelines(htrc6)
+with open('../data/htrc/all.csv', 'w') as newfile:
+    writer = csv.DictWriter(newfile, ['id', 'lccn'], extrasaction='ignore')
+    for row in htrc6:
+        writer.writerow(row)
+    for row in htrc86:
+        writer.writerow(row)
+
