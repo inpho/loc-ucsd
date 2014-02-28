4e06a6e7f86519c98d73fc7c8fb39767dd91ee69
Jaimie Murdock <jaimie.murdock@gmail.com>
2014-02-28 13:33:27 -0500

    adding new lines

--- scripts/make_csv.py
+++ scripts/make_csv.py
@@ -3,12 +3,12 @@ import json
 
 htrc1314 = []
 htrc86 = []
-htrc6=["uc2.ark+=13960=t5w66bs1h",
-"uc2.ark+=13960=t6057f659",
-"uc2.ark+=13960=t74t6gs0m",
-"uc2.ark+=13960=t0ht2h954",
-"uc2.ark+=13960=t05x26n0d",
-"uc2.ark+=13960=t5p84550z"]
+htrc6=["uc2.ark+=13960=t5w66bs1h\n",
+"uc2.ark+=13960=t6057f659\n",
+"uc2.ark+=13960=t74t6gs0m\n",
+"uc2.ark+=13960=t0ht2h954\n",
+"uc2.ark+=13960=t05x26n0d\n",
+"uc2.ark+=13960=t5p84550z\n"]
 
 with open('htrc86.json') as jsonfile:
     data = json.load(jsonfile)
@@ -17,9 +17,9 @@ with open('htrc86.json') as jsonfile:
 with open('../data/csv/htrc_lcco.csv', 'rb') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        htrc1314.append(row['id'])
+        htrc1314.append(row['id'] + '\n')
         if row['id'] in volumes:
-            htrc86.append(row['id'])
+            htrc86.append(row['id'] + '\n')
 
 print len(htrc1314), len(htrc86), len(htrc6)
 
